d1bd4651f2c62aa3f8055fb1c2f71f3dc747b87e
Felix Mueller <felix.max.mueller@gmail.com>
2024-02-04 18:00:48 +0100

    center content

---
+++ src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import { Box, Stack } from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <Stack
+      alignItems="center"
+      sx={{
+        backgroundColor: "whitesmoke",
+        height: "100vh",
+        overflowY: "auto",
+      }}
+    >
+      <Box sx={{ width: "100%", maxWidth: 1000, backgroundColor: "white" }}>
+        <Outlet />
+      </Box>
+    </Stack>
+  );
+}
+
+export default Layout;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,5 @@
 import { Navigate, createBrowserRouter } from "react-router-dom";
+import Layout from "../components/Layout";
 import RecipeDetailPage from "../pages/recipeDetail";
 import RecipeListPage from "../pages/recipeList";
 import routes from "./routes";
@@ -6,15 +7,21 @@ import routes from "./routes";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to={routes.recipeList} />,
-  },
-  {
-    path: routes.recipeList,
-    element: <RecipeListPage />,
-  },
-  {
-    path: routes.recipeDetail(":recipeId"),
-    element: <RecipeDetailPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to={routes.recipeList} />,
+      },
+      {
+        path: routes.recipeList,
+        element: <RecipeListPage />,
+      },
+      {
+        path: routes.recipeDetail(":recipeId"),
+        element: <RecipeDetailPage />,
+      },
+    ],
   },
 ]);
 
