ff5db0e4f4c777be47bbcefa6519955563814a18
Müller, Felix - D0344891 <felix.mueller@dm.de>
2024-02-10 15:39:23 +0100

    Add search bar logic

--- src/pages/recipeList/index.tsx
+++ src/pages/recipeList/index.tsx
@@ -2,19 +2,24 @@ import { Box, Divider } from "@mui/material";
 import RecipePreview from "../../sections/recipeList/RecipePreview";
 import Toolbar from "../../sections/recipeList/Toolbar";
 import testData from "../../testData";
+import { useState } from "react";
 
 function RecipeListPage() {
+  const [searchText, setSearchText] = useState("");
+
   const recipes = testData;
 
   return (
     <Box>
-      <Toolbar />
-      {recipes.map((recipe) => (
-        <Box key={recipe.id}>
-          <Divider />
-          <RecipePreview recipe={recipe} />
-        </Box>
-      ))}
+      <Toolbar textFieldValue={searchText} setTextFieldValue={setSearchText} />
+      {recipes
+        .filter((recipe) => recipe.title.includes(searchText))
+        .map((recipe) => (
+          <Box key={recipe.id}>
+            <Divider />
+            <RecipePreview recipe={recipe} />
+          </Box>
+        ))}
     </Box>
   );
 }
--- src/sections/recipeList/Toolbar.tsx
+++ src/sections/recipeList/Toolbar.tsx
@@ -1,10 +1,18 @@
 import { Search, Tune } from "@mui/icons-material";
 import { Button, InputAdornment, Stack, TextField } from "@mui/material";
+import React from "react";
 
-function Toolbar() {
+type PropType = {
+  textFieldValue: string;
+  setTextFieldValue: React.Dispatch<React.SetStateAction<string>>;
+};
+
+function Toolbar({ textFieldValue, setTextFieldValue }: PropType) {
   return (
     <Stack direction="row" justifyContent="space-between" sx={{ p: 3 }}>
       <TextField
+        value={textFieldValue}
+        onChange={(e) => setTextFieldValue(e.target.value)}
         variant="outlined"
         sx={{ pr: 3 }}
         InputProps={{
