226bd829d4e5b89a3937858ba4e98db6079c492c
Felix Müller <felix.mueller@dm.de>
2025-05-28 21:01:58 +0200

    React Query initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import theme from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <React.StrictMode>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </React.StrictMode>
   );
 }
